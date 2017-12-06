dd39a1504ebc00f39bc49f8d3ea6f1f92db04edb
ripee <ripee.dev@gmail.com>
2017-12-06 18:00:07 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,14 +24,12 @@ TARGET_CPU_VARIANT := generic
 TARGET_CPU_SMP := true
 
 # Boot image
-TARGET_RECOVERY_FSTAB := $(DEVICE_TREE)/recovery/root/fstab.universal5433
 TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/kernel
-BOARD_KERNEL_CMDLINE := # Exynos doesn't take cmdline arguments from boot image
-BOARD_KERNEL_BASE := 0x10000000
-BOARD_KERNEL_PAGESIZE := 2048
-# 000RU = recovery kernel, 000KU = system kernel
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SYSMAGIC000RU
 BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_TREE)/mkbootimg.mk
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x105C0000
